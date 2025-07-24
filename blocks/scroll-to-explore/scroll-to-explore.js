dd166d2202d47464b4771bee3339a482bdc05442
Satvika Reddy Kasireddy <skasireddy@worldbankgroup.org>
2025-07-24 16:04:41 +0530

    scroll to explore block

---
+++ blocks/scroll-to-explore/scroll-to-explore.js
@@ -0,0 +1,20 @@
+export default function decorate(block) {
+  console.log('test video block', block);
+  const btnContainer = block.querySelector('.button-container');
+  if (btnContainer) {
+    const scrollIndicator = document.createElement('a');
+    scrollIndicator.className = 'scroll-indicator';
+    const spanElement = document.createElement('span')
+    spanElement.className = 'lp lp=chevron-down'
+    spanElement.addEventListener('click', () => {
+        //logic on scroll icon click
+    });
+    const divElement = document.createElement('div')
+    divElement.innerText = 'Scroll to explore'
+    scrollIndicator.appendChild(divElement);
+    scrollIndicator.appendChild(spanElement);
+    // Replace the old button container with the new scroll indicator
+    btnContainer.replaceWith(scrollIndicator);
+  }
+}
+
