08e491b4c595de08dc84161e02eef10a55a75e61
Satvika Reddy Kasireddy <skasireddy@worldbankgroup.org>
2025-07-24 16:09:08 +0530

    resolve lint errors

--- blocks/scroll-to-explore/scroll-to-explore.js
+++ blocks/scroll-to-explore/scroll-to-explore.js
@@ -4,17 +4,16 @@ export default function decorate(block) {
   if (btnContainer) {
     const scrollIndicator = document.createElement('a');
     scrollIndicator.className = 'scroll-indicator';
-    const spanElement = document.createElement('span')
-    spanElement.className = 'lp lp-chevron-down'
+    const spanElement = document.createElement('span');
+    spanElement.className = 'lp lp-chevron-down';
     spanElement.addEventListener('click', () => {
         //logic on scroll icon click
-    });
-    const divElement = document.createElement('div')
-    divElement.innerText = 'Scroll to explore'
+});
+    const divElement = document.createElement('div');
+    divElement.innerText = 'Scroll to explore';
     scrollIndicator.appendChild(divElement);
     scrollIndicator.appendChild(spanElement);
     // Replace the old button container with the new scroll indicator
     btnContainer.replaceWith(scrollIndicator);
   }
 }
-
